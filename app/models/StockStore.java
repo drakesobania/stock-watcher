17ecd9c9ebf210117c9e837ef6481580470c6a31
Drake Sobania <dsobania3@gatech.edu>
2021-09-18 23:42:12 -0400

    minor fix

--- app/models/StockStore.java
+++ app/models/StockStore.java
@@ -3,14 +3,20 @@ package models;
 import java.util.*;
 
 public class StockStore {
-    private Set<Stock> stocks = new HashSet<>();
+    private HashMap<Integer, Stock> stocks = new HashMap<>();
 
     public Optional<Stock> add(Stock stock) {
-        stocks.add(stock);
+        stocks.put(stocks.size(), stock);
         return Optional.ofNullable(stock);
     }
 
     public boolean remove(Stock stock) {
-        return stocks.remove(stock);
+        for (Integer key: stocks.keySet()) {
+            if (stocks.get(key).getSymbol().equals(stock.getSymbol())) {
+                stocks.remove(key);
+                return true;
+            }
+        }
+        return true;
     }
 }
